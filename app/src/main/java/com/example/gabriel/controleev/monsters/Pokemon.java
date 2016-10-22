cd9bcc7555dd794f44e47bcebbcf98a91e6dd75f
Gabriel Lecomte <gabriel1906@gmail.com>
2016-10-22 00:28:24 -0300

    Acrescenta getters e setters a Pokemon

--- app/src/main/java/com/example/gabriel/controleev/monsters/Pokemon.java
+++ app/src/main/java/com/example/gabriel/controleev/monsters/Pokemon.java
@@ -16,4 +16,52 @@ public class Pokemon extends PokedexEntry {
         this.speed = speed;
         this.hp = hp;
     }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpAttack() {
+        return spAttack;
+    }
+
+    public void setSpAttack(int spAttack) {
+        this.spAttack = spAttack;
+    }
+
+    public int getSpDefense() {
+        return spDefense;
+    }
+
+    public void setSpDefense(int spDefense) {
+        this.spDefense = spDefense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
